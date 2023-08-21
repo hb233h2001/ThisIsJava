eff2e3c369231f2f68926d6db55a1353fe5d6636
hb233h2001 <129055894+hb233h2001@users.noreply.github.com>
2023-08-21 19:00:53 +0900

    Add files via upload

---
+++ src/ch07/sec10/exam01/PhoneExample.java
@@ -0,0 +1,13 @@
+package ch07.sec10.exam01;
+
+public class PhoneExample {
+    public static void main(String[] args) {
+        //Phone phone = new phone();
+
+        SmartPhone smartPhone = new SmartPhone("홍길동");
+
+        smartPhone.turnOn();
+        smartPhone.internetSearch();
+        smartPhone.turnOff();
+    }
+}
